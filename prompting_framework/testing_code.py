f46eec47edb62d9923cf90399f6df9ec0b245541
S M A Tousi <mohamadali.tousi@gmail.com>
2024-10-11 17:31:46 -0500

    testing the prompting

--- prompting_framework/testing_code.py
+++ prompting_framework/testing_code.py
@@ -3,7 +3,7 @@ import ollama
 response = ollama.chat(model='phi3', messages=[
   {
     'role': 'user',
-    'content': 'Why is the sky blue?',
+    'content': 'what did I just ask you?',
   },
 ])
 print(response['message']['content'])
