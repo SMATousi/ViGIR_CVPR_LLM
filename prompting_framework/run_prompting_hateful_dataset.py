a1851daca5de9dbb7a2dad240a62e1e714c079f7
S M A Tousi <mohamadali.tousi@gmail.com>
2024-10-16 09:54:09 -0500

    timeout duration added

--- prompting_framework/run_prompting_hateful_dataset.py
+++ prompting_framework/run_prompting_hateful_dataset.py
@@ -2,11 +2,9 @@ import ollama
 import os
 from tqdm import tqdm
 import json
+import signal
 
 
-images_path = '/home1/pupil/goowfd/CVPR_2025/hateful_memes/img'
-
-list_of_image_names = os.listdir(images_path)
 
 def check_yes_no(text):
     # Strip any leading/trailing whitespace and convert to lowercase
@@ -20,14 +18,36 @@ def check_yes_no(text):
     else:
         return None
 
+def check_yes_no(text):
+    # Strip any leading/trailing whitespace and convert to lowercase
+    text = text.strip().lower()
 
+    # Check if the text starts with 'yes' or 'no'
+    if text.startswith("yes"):
+        return 1
+    elif text.startswith("no"):
+        return 0
+    else:
+        return None  
+    
+class TimeoutException(Exception):
+    pass
+
+def timeout_handler(signum, frame):
+    raise TimeoutException
+
+
+
+root_path = '/home1/pupil/goowfd/CVPR_2025/hateful_memes/img'
+
+list_of_image_names = os.listdir(root_path)
 
 model_name = 'llava:7b'
 results_file_name = model_name + '_results_hateful.json'
 ollama.pull(model_name)
 
-root_path = '/home1/pupil/goowfd/CVPR_2025/hateful_memes/img'
-
+timeout_duration = 20
+print(f"Handling the timeout exceptions with timeout duration of {timeout_duration} seconds")
 options= {  # new
             "seed": 123,
             "temperature": 0,
@@ -45,10 +65,22 @@ for image_name in tqdm(list_of_image_names):
 #     prompt = "Is this somehow an offensive meme? Please answer with YES or NO: "
 #     prompt = "describe this image: "
     
-    response = ollama.generate(model='llava:7b', prompt=prompt, images=[image_path], options=options)
-    
+    signal.signal(signal.SIGALRM, timeout_handler)
+    signal.alarm(timeout_duration)  # Set the timeout
 
-    label = check_yes_no(response['response'])
+    try:
+        response = ollama.generate(model='llava:7b', prompt=prompt, images=[image_path], options=options)
+
+        label = check_yes_no(response['response'])
+
+    except TimeoutException:
+        print(f"Prompt for {image_name} took longer than {timeout_duration} seconds. Moving to the next one.")
+        label = None
+
+    finally:
+        signal.alarm(0)  # Disable the alarm
+
+    
     
 #     print(label)
     
