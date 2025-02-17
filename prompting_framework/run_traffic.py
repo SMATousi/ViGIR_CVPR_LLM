f0a6b9ea197c7e14235ae535299cbb9a5e0192e5
ali <mohamadali.tousi@gmail.com>
2025-02-17 11:22:28 -0600

    embedding corrected

--- prompting_framework/run_traffic.py
+++ prompting_framework/run_traffic.py
@@ -97,11 +97,11 @@ args = parser.parse_args()
 #dataset_name = "traffic"
 #model_unloading= True
 
-# run = wandb.init(
-#     # entity="jacketdembys",
-#     project=f"CVPR-2025-Traffic",
-#     name="run_test_traffic_"+args.model_name+"-"+args.subset
-# )
+run = wandb.init(
+    # entity="jacketdembys",
+    project=f"CVPR-2025-Traffic",
+    name="run_test_traffic_"+args.model_name+"-"+args.subset
+)
 
 if args.dataset_name == "traffic":
     class_names = "Speed limit (20km/h), Speed limit (30km/h), Speed limit (50km/h), Speed limit (60km/h), Speed limit (70km/h), Speed limit (80km/h), End of speed limit (80km/h), Speed limit (100km/h), Speed limit (120km/h), No passing, No passing for vehicles over 3.5 metric tons, Right-of-way at the next intersection, Priority road, Yield, Stop, No vehicles, Vehicles over 3.5 metric tons prohibited, No entry, General caution, Dangerous curve to the left, Dangerous curve to the right, Double curve, Bumpy road, Slippery road, Road narrows on the right, Road work, Traffic signals, Pedestrians, Children crossing, Bicycles crossing, Beware of ice/snow, Wild animals crossing, End of all speed and passing limits, Turn right ahead, Turn left ahead, Ahead only, Go straight or right, Go straight or left, Keep right, Keep left, Roundabout mandatory, End of no passing, End of no passing by vehicles over 3.5 metric tons"
@@ -213,21 +213,21 @@ if args.dataset_name == "traffic":
             "model_response": model_response # string coming from the model
         }
 
-        print(model_labels)
+        # print(model_labels)
 
-    with open(results_file_name, 'w') as fp:
-        json.dump(model_labels, fp, indent=4)
+        with open(results_file_name, 'w') as fp:
+            json.dump(model_labels, fp, indent=4)
 
-    with open(raw_image_info, 'w') as fp:
-        json.dump(data, fp, indent=4)
+        with open(raw_image_info, 'w') as fp:
+            json.dump(data, fp, indent=4)
 
 
 
-    # Optionally, if you want to save the JSON as an artifact
-    artifact = wandb.Artifact("json_file", type="dataset")
-    artifact.add_file(results_file_name)
-    wandb.log_artifact(artifact)
+        # Optionally, if you want to save the JSON as an artifact
+        artifact = wandb.Artifact("json_file", type="dataset")
+        artifact.add_file(results_file_name)
+        wandb.log_artifact(artifact)
 
-    artifact = wandb.Artifact("json_file", type="dataset")
-    artifact.add_file(raw_image_info)
-    wandb.log_artifact(artifact)
+        artifact = wandb.Artifact("json_file", type="dataset")
+        artifact.add_file(raw_image_info)
+        wandb.log_artifact(artifact)
